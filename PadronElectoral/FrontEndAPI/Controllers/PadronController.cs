3842a13d1aa7cd3d97566a60975dd2d77231f676
JONATHAN RAMIREZ <jonathan-ramirezz@hotmail.com>
2021-08-03 22:24:34 -0600

    api

---
+++ PadronElectoral/FrontEndAPI/Controllers/PadronController.cs
@@ -0,0 +1,46 @@
+﻿using BackEnd.DAL;
+using BackEnd.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEndAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PadronController : ControllerBase
+    {
+        // GET: api/<PadronController>
+        [HttpGet]
+        public JsonResult Get()
+        {
+            IEnumerable<PadronCompleto> padronCompletoList;
+            using (UnidadDeTrabajo<PadronCompleto> unidad = new UnidadDeTrabajo<PadronCompleto>(new Padron_ElectoralContext()))
+            {
+                padronCompletoList = unidad.genericDAL.GetAll();
+            }
+
+
+            return new JsonResult(padronCompletoList);
+        }
+
+        // GET api/<PadronController>/5
+        [HttpGet("{cedula}")]
+        public JsonResult Get(int cedula)
+        {
+            PadronCompleto padronCompleto;
+            using (UnidadDeTrabajo<PadronCompleto> unidad = new UnidadDeTrabajo<PadronCompleto>(new Padron_ElectoralContext()))
+            {
+                padronCompleto = unidad.genericDAL.Get(cedula);
+            }
+
+            return new JsonResult(padronCompleto);
+        }
+
+
+    }
+}
---
+++ PadronElectoral/PadronElectoral/Entities/PadronCompleto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace BackEnd.Entities
+{
+    public partial class PadronCompleto
+    {
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public string Correo { get; set; }
+    }
+}
